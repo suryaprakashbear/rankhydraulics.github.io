db1c69f59e880fd93728751464699a5119c5a876
dentalacare <42679146+suryaprakashbear@users.noreply.github.com>
2022-05-17 15:47:08 +0530

    commit changes

---
+++ rank.js
@@ -0,0 +1,8 @@
+function menuShow() {
+	document.getElementById("menu").classList.toggle("show");
+	
+}
+function menuClose() {
+	document.getElementById("menu").classList.remove("show");
+}
+/******************* collapse dropdown closing ***************************/
